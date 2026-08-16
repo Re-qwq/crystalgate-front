e34584edf0c03173c106e97358b98dc4d92fe792
Re-qwq <3829526785@qq.com>
2026-08-16 13:47:01 +0800

    v1.7.4: 后备地址列表更新

--- js/backend-config.js
+++ js/backend-config.js
@@ -2,6 +2,7 @@
 // 注意: 不依赖 Gist/任何第三方 (避免国内网络被墙)
 window.__CG_BACKENDS = [
     "https://wptvo-47-93-224-81.run.pinggy-free.link/api/v2",
+    "https://e66397ede592a3d6-47-93-224-81.serveousercontent.com/api/v2",
     "https://5ab2a9567c924a69-47-93-224-81.serveousercontent.com/api/v2",
     "https://e600983aa6a949a6-47-93-224-81.serveousercontent.com/api/v2"
 ];
