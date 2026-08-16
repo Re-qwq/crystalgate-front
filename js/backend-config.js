04349965e156e43edf0df8111e5f12485974eb83
Re-qwq <3829526785@qq.com>
2026-08-16 14:40:44 +0800

    backend-config: 刷新地址列表

--- js/backend-config.js
+++ js/backend-config.js
@@ -2,6 +2,5 @@
 // 注意: 不依赖 Gist/任何第三方 (避免国内网络被墙)
 window.__CG_BACKENDS = [
     "https://oqkzd-47-93-224-81.run.pinggy-free.link/api/v2",
-    "https://275709ac7f4769e7-47-93-224-81.serveousercontent.com/api/v2",
-    "https://aa5c94e47e1d6349-47-93-224-81.serveousercontent.com/api/v2"
+    "https://a22db562a4ed28fc-47-93-224-81.serveousercontent.com/api/v2"
 ];
