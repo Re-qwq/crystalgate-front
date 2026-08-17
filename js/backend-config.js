0b90adf987c1218eb93edc6c871657d71bc3290c
Re-qwq <3829526785@qq.com>
2026-08-17 12:41:51 +0800

    backend-config: 当前隧道

--- js/backend-config.js
+++ js/backend-config.js
@@ -1,4 +1,4 @@
 // CrystalGate 后端地址配置 (由守护脚本自动更新, 前端启动时探测存活)
 window.__CG_BACKENDS = [
-    "https://71caa136f186c3fd-47-93-224-81.serveousercontent.com/api/v2"
+    "https://e41f0a0f478e05a6-47-93-224-81.serveousercontent.com/api/v2"
 ];
