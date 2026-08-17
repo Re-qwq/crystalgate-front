e55956a8689886dac07a5c6971b871b670d41846
络空 <3829526785@qq.com>
2026-08-17 06:34:49 +0000

    v1.8.2: js/backend-config.js

--- js/backend-config.js
+++ js/backend-config.js
@@ -1,4 +1,4 @@
 // CrystalGate 后端地址配置 (由守护脚本自动更新, 前端启动时探测存活)
 window.__CG_BACKENDS = [
-    "https://e41f0a0f478e05a6-47-93-224-81.serveousercontent.com/api/v2"
+    "https://a8aa6fa97eac5ff5-47-93-224-81.serveousercontent.com/api/v2"
 ];
