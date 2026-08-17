5382889f74c2fd854d2a28cff07d3eda8c0a9baa
络空 <3829526785@qq.com>
2026-08-17 09:04:21 +0000

    backend-config: tunnel update

--- js/backend-config.js
+++ js/backend-config.js
@@ -1,4 +1,4 @@
 // CrystalGate 后端地址配置 (由守护脚本自动更新, 前端启动时探测存活)
 window.__CG_BACKENDS = [
-    "https://a8aa6fa97eac5ff5-47-93-224-81.serveousercontent.com/api/v2"
+    "https://f6695a0913834bf0-47-93-224-81.serveousercontent.com/api/v2"
 ];
