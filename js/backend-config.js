3dc773cc380d36422f524cf0e6a1a5b220bf6deb
络空 <3829526785@qq.com>
2026-08-24 16:10:54 +0800

    v1.8.5: 更新隧道地址: js/backend-config.js

--- js/backend-config.js
+++ js/backend-config.js
@@ -1,6 +1,5 @@
 // CrystalGate 后端地址配置 (由守护脚本自动更新, 前端启动时探测存活)
 window.__CG_BACKENDS = [
-    "https://implement-conjunction-economics-concerned.trycloudflare.com/api/v2",
-    "https://eddno-49-234-206-157.run.pinggy-free.link/api/v2",
-    "https://iizcr-49-235-96-207.free.pinggy.net/api/v2"
+    "https://ion-detail-throws-acer.trycloudflare.com/api/v2",
+    "https://nogcy-150-158-160-124.run.pinggy-free.link/api/v2"
 ];
